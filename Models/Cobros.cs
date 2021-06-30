681fd22220a9769594c3fd4707c9724cae1ea125
Crismeyri Gutierrez Gil <crismeyri_gutierrez@ucne.edu.do>
2021-06-30 18:12:36 -0400

    Creando la entidad cobros

--- Models/Cobros.cs
+++ Models/Cobros.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +9,15 @@ namespace CrismeyriP2_AP2.Models
 {
     public class Cobros
     {
+        [Key]
+        public int CobroId { get; set; }
+        public DateTime Fecha { get; set; }
+        public int ClienteId { get; set; }
+        [Range(minimum: 1, maximum: int.MaxValue, ErrorMessage = "Debe realizar un cobro")]
+        public double TotalCobrado { get; set; }
+        public string Observaciones { get; set; }
+
+        [ForeignKey("CobroId")]
+        public virtual List<CobrosDetalle> Detalle { get; set; } = new List<CobrosDetalle>();
     }
 }
