386612696c0479c3e5c27ae91e4531a490fc5f5a
Crismeyri Gutierrez Gil <crismeyri_gutierrez@ucne.edu.do>
2021-06-30 18:26:19 -0400

    Creando la BLL Clientes

---
+++ BLL/ClientesBLL.cs
@@ -0,0 +1,34 @@
+﻿using CrismeyriP2_AP2.DAL;
+using CrismeyriP2_AP2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace CrismeyriP2_AP2.BLL
+{
+    public class ClientesBLL
+    {
+        public static List<Clientes> GetList(Expression<Func<Clientes, bool>> cliente)
+        {
+            List<Clientes> Lista = new List<Clientes>();
+            Contexto contexto = new Contexto();
+
+            try
+            {
+                Lista = contexto.Clientes.Where(cliente).ToList();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+            return Lista;
+        }
+    }
+}
+
